2d098b75ab2bd2dcb017a206909d99007c09b959
aadya <aadyasingh006@gmail.com>
2021-11-08 15:09:24 -0600

    Made ForbiddenIslandRunner.java

--- src/ForbiddenIslandRunner.java
+++ src/ForbiddenIslandRunner.java
@@ -1,4 +1,4 @@
-public class Main {
+public class ForbiddenIslandRunner {
     public static void main(String[] args) {
         System.out.println("Lets actually Start now");
         System.out.println("damnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn");
