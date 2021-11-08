249d9cf4c8656154b3a66a56d7406bf9a6ac7a43
Masaab <abcdxyz@myhouse.com>
2021-11-08 15:21:57 -0600

    pog

--- src/ForbiddenIslandRunner.java
+++ src/ForbiddenIslandRunner.java
@@ -1,6 +1,5 @@
 public class ForbiddenIslandRunner {
     public static void main(String[] args) {
         System.out.println("Lets actually Start now");
-        System.out.println("Test World");
     }
 }
