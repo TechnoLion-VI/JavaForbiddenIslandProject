891c69d83ed3c7d7db06b0a215a6470efbdbb334
Masaab <abcdxyz@myhouse.com>
2021-11-08 15:12:56 -0600

    we are actually starting now

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Lets actually Start now");
-        System.out.println("damnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn");
+        System.out.println("Test World");
     }
 }
