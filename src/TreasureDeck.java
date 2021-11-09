6d9b2b30a3e55655f891f602f4fba3773386cca2
Breadyoactive <bradydaboss101@gmail.com>
2021-11-09 07:26:26 -0600

    Coded TreasureDeck poggers

--- src/TreasureDeck.java
+++ src/TreasureDeck.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class TreasureDeck {
+    private ArrayList<TreasureCard> deck;
+    private ArrayList<TreasureCard> discard;
+    public TreasureDeck(int seed) {
+
+    }
+    public void shuffle() {
+        Collections.shuffle(discard);
+        deck = discard;
+        discard.clear();
+    }
 }
