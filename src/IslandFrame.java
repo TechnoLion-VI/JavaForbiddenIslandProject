69c8ff199b912e24fbc4dfb4c311628abf68f3be
aadya <aadyasingh006@gmail.com>
2021-11-09 07:23:28 -0600

    Made ForbiddenIslandRunner.java ......

--- src/IslandFrame.java
+++ src/IslandFrame.java
@@ -1,2 +1,16 @@
-public class IslandFrame {
+import javax.swing.*;
+
+public class IslandFrame extends JFrame {
+    private static final int WIDTH = 1200;
+    private static final int HEIGHT = 480;
+
+    public IslandFrame(String framename) {
+        super(framename);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(WIDTH, HEIGHT);
+        setVisible(true);
+    }
 }
+
+
+
