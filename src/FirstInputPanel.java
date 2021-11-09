2c96d72f7f3bafad3afd3274aa2c218f23d78379
Masaab <abcdxyz@myhouse.com>
2021-11-09 16:22:19 -0600

    got the bg working

--- src/FirstInputPanel.java
+++ src/FirstInputPanel.java
@@ -1,19 +1,21 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.image.*;
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.io.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.Objects;
 
-public class FirstInputPanel extends JPanel {
-    private BufferedImage background;
+public class FirstInputPanel extends JPanel{
+    private BufferedImage ForbiddenIslandBG;
 
     public FirstInputPanel() {
-        background = (BufferedImage) Toolkit.getDefaultToolkit().createImage("forbidden-island-background.jpg");
-
-
+        try {
+            this.ForbiddenIslandBG= ImageIO.read(Objects.requireNonNull(FirstInputPanel.class.getResource("Images/forbidden-island-background.jpg")));
+        } catch (Exception E) {
+            System.out.println("Exception Error");
+        }
     }
+
     public void paint(Graphics g) {
-        g.drawImage(background, 0, 0, null);
+        g.drawImage(this.ForbiddenIslandBG, 0, 0, 1285, 670, null);
     }
 }
