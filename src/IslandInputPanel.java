d1e37c647addf12af31d06b75286088b0860a6d3
Breadyoactive <bradydaboss101@gmail.com>
2021-11-10 07:57:56 -0600

    help frame thing

--- src/IslandInputPanel.java
+++ src/IslandInputPanel.java
@@ -5,19 +5,20 @@ import java.awt.image.BufferedImage;
 import java.util.Objects;
 
 public class IslandInputPanel extends JPanel{
-    private BufferedImage ForbiddenIslandBG;
+    private BufferedImage ForbiddenIslandBG, FirstScreen;
 
     public IslandInputPanel() {
         try {
             //Commented out original bg
             //this.ForbiddenIslandBG= ImageIO.read(Objects.requireNonNull(IslandInputPanel.class.getResource("Images/forbidden-island-background.jpg")));
             this.ForbiddenIslandBG= ImageIO.read(Objects.requireNonNull(IslandInputPanel.class.getResource("Images/Possible Forbidden Island BG.jpg")));
+            this.FirstScreen= ImageIO.read(Objects.requireNonNull(IslandInputPanel.class.getResource("Images/FirstScreen.png")));
         } catch (Exception E) {
-            System.out.println("Exception Error");
+            System.out.println(E);
         }
     }
 
     public void paint(Graphics g) {
-        g.drawImage(this.ForbiddenIslandBG, 0, 0, 1920, 1080, null);
+        g.drawImage(this.FirstScreen, 0, 0, 1920, 1080, null);
     }
 }
