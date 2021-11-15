952a63336c82c0e095f095ee04ddb1f44f20021d
AadyaSing <90815986+AadyaSing@users.noreply.github.com>
2021-11-15 11:39:05 -0600

    I put images in the arraylist- i tried my best, im pretty sure theres some other way to do this cuz this shit took me a long time

--- src/IslandPanel.java
+++ src/IslandPanel.java
@@ -1,26 +1,86 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Objects;
 
 
-public class IslandPanel extends JPanel implements ActionListener {
-    private String NumOfPlayers[] = {"2", "3", "4"};
-    private String waterLevel[] = {"Novice", "Normal", "Intermediate", "Legendary"};
-    private JTextField textfield1 = new JTextField ("Choose the number of players : ");
-    private JComboBox amount = new JComboBox();
-    private int count = 0;
+public class IslandPanel extends JPanel {
+    private BufferedImage one, two, three, four, fifth, sixth, seventh, eighth, ninth, tenth, eleventh, twelvth, thirteen, fourteen, fifteen, sixteen, seventeen, eighteen, nineteen, twenty, twentyone, twentytwo, twentythree, twentyfour;
+    private ArrayList<BufferedImage> Islands = new <BufferedImage> ArrayList();
 
     public IslandPanel() {
-        for(int i = 0; i < NumOfPlayers.length; i++)
-            amount.addItem(NumOfPlayers[count++]);
-        textfield1.setEditable(false);
+        try {
+            one = ImageIO.read(Objects.requireNonNull(IslandPanel.class.getResource("Images/Flood_Card_Breakers Bridge@2x.png")));
+            two = ImageIO.read(Objects.requireNonNull(IslandPanel.class.getResource("Images/Flood_Card_Bronze Gate@2x.png")));
+            three = ImageIO.read(Objects.requireNonNull(IslandPanel.class.getResource("Images/Flood_Card_Cave Of Embers@2x.png")));
+            four = ImageIO.read(Objects.requireNonNull(IslandPanel.class.getResource("Images/Flood_Card_Cave Of Shadows@2x.png")));
+            fifth = ImageIO.read(Objects.requireNonNull(IslandPanel.class.getResource("Images/Flood_Card_Cliffs Of Abandon@2x.png")));
+            sixth = ImageIO.read(Objects.requireNonNull(IslandPanel.class.getResource("Images/Flood_Card_Copper Gate@2x.png")));
+            seventh = ImageIO.read(Objects.requireNonNull(IslandPanel.class.getResource("Images/Flood_Card_Coral Palace@2x.png")));
+            eighth = ImageIO.read(Objects.requireNonNull(IslandPanel.class.getResource("Images/Flood_Card_Crimson Forest@2x.png")));
+            ninth = ImageIO.read(Objects.requireNonNull(IslandPanel.class.getResource("Images/Flood_Card_Dunes Of Deception@2x.png")));
+            tenth = ImageIO.read(Objects.requireNonNull(IslandPanel.class.getResource("Images/Flood_Card_Fools_Landing@2x.png")));
+            eleventh = ImageIO.read(Objects.requireNonNull(IslandPanel.class.getResource("Images/Flood_Card_Gold Gate@2x.png")));
+            twelvth = ImageIO.read(Objects.requireNonNull(IslandPanel.class.getResource("Images/Flood_Card_Howling Garden@2x.png")));
+            thirteen = ImageIO.read(Objects.requireNonNull(IslandPanel.class.getResource("Images/Flood_Card_Iron Gate@2x.png")));
+            fourteen = ImageIO.read(Objects.requireNonNull(IslandPanel.class.getResource("Images/Flood_Card_Lost Lagoon@2x.png")));
+            fifteen = ImageIO.read(Objects.requireNonNull(IslandPanel.class.getResource("Images/Flood_Card_Misty Marsh@2x.png")));
+            sixteen = ImageIO.read(Objects.requireNonNull(IslandPanel.class.getResource("Images/Flood_Card_Observatory@2x.png")));
+            seventeen = ImageIO.read(Objects.requireNonNull(IslandPanel.class.getResource("Images/Flood_Card_Phantom Rock@2x.png")));
+            eighteen = ImageIO.read(Objects.requireNonNull(IslandPanel.class.getResource("Images/Flood_Card_Silver Gate@2x.png")));
+            nineteen = ImageIO.read(Objects.requireNonNull(IslandPanel.class.getResource("Images/Flood_Card_Temple of the Moon@2x.png")));
+            twenty = ImageIO.read(Objects.requireNonNull(IslandPanel.class.getResource("Images/Flood_Card_Temple of the Sun@2x.png")));
+            twentyone = ImageIO.read(Objects.requireNonNull(IslandPanel.class.getResource("Images/Flood_Card_Tidal Palace@2x.png")));
+            twentytwo = ImageIO.read(Objects.requireNonNull(IslandPanel.class.getResource("Images/Flood_Card_Twilight Hollow@2x.png")));
+            twentythree = ImageIO.read(Objects.requireNonNull(IslandPanel.class.getResource("Images/Flood_Card_Watchtower@2x.png")));
+            twentyfour = ImageIO.read(Objects.requireNonNull(IslandPanel.class.getResource("Images/Flood_Card_Whispering Garden@2x.png")));
+
+            Islands.add(one);
+            Islands.add(two);
+            Islands.add(three);
+            Islands.add(four);
+            Islands.add(fifth);
+            Islands.add(sixth);
+            Islands.add(seventh);
+            Islands.add(eighth);
+            Islands.add(ninth);
+            Islands.add(tenth);
+            Islands.add(eleventh);
+            Islands.add(twelvth);
+            Islands.add(thirteen);
+            Islands.add(fourteen);
+            Islands.add(fifteen);
+            Islands.add(sixteen);
+            Islands.add(seventeen);
+            Islands.add(eighteen);
+            Islands.add(nineteen);
+            Islands.add(twenty);
+            Islands.add(twentyone);
+            Islands.add(twentytwo);
+            Islands.add(twentythree);
+            Islands.add(twentyfour);
+
+
+
+
+
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
 
 
 
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
-    }
-}
