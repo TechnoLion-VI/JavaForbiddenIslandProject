762ba1c23fd82efefd44d6dd6310e4796d8d186a
aadya <aadyasingh006@gmail.com>
2021-11-15 07:20:58 -0600

    update aint working for me

--- src/IslandPanel.java
+++ src/IslandPanel.java
@@ -3,24 +3,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 
-public class IslandPanel extends JPanel implements ActionListener {
-    private String NumOfPlayers[] = {"2", "3", "4"};
-    private String waterLevel[] = {"Novice", "Normal", "Intermediate", "Legendary"};
-    private JTextField textfield1 = new JTextField ("Choose the number of players : ");
-    private JComboBox amount = new JComboBox();
-    private int count = 0;
+public class IslandPanel extends JPanel {
 
-    public IslandPanel() {
-        for(int i = 0; i < NumOfPlayers.length; i++)
-            amount.addItem(NumOfPlayers[count++]);
-        textfield1.setEditable(false);
-
-
-
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
-    }
 }
