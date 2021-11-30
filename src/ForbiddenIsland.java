f3ba78c4df81515df9b0cc561ee668d68ac9d8dd
Breadyoactive <90989462+Breadyoactive@users.noreply.github.com>
2021-11-30 08:04:07 -0600

    im dying

--- src/ForbiddenIsland.java
+++ src/ForbiddenIsland.java
@@ -9,6 +9,30 @@ public class ForbiddenIsland {
         frame2 = new IslandFrame("Forbidden Island");
         ArrayList<IslandTile> board = new ArrayList<>();
         IslandTile BreakersBridge = new IslandTile(0, 0, null, "Breakers Bridge", frame2.getImage(0));
+        IslandTile BreakersBridge = new IslandTile(0, 0, null, "Breakers Bridge", frame2.getImage(1));
+        IslandTile BreakersBridge = new IslandTile(0, 0, null, "Breakers Bridge", frame2.getImage(2));
+        IslandTile BreakersBridge = new IslandTile(0, 0, null, "Breakers Bridge", frame2.getImage(3));
+        IslandTile BreakersBridge = new IslandTile(0, 0, null, "Breakers Bridge", frame2.getImage(4));
+        IslandTile BreakersBridge = new IslandTile(0, 0, null, "Breakers Bridge", frame2.getImage(5));
+        IslandTile BreakersBridge = new IslandTile(0, 0, null, "Breakers Bridge", frame2.getImage(6));
+        IslandTile BreakersBridge = new IslandTile(0, 0, null, "Breakers Bridge", frame2.getImage(7));
+        IslandTile BreakersBridge = new IslandTile(0, 0, null, "Breakers Bridge", frame2.getImage(8));
+        IslandTile BreakersBridge = new IslandTile(0, 0, null, "Breakers Bridge", frame2.getImage(9));
+        IslandTile BreakersBridge = new IslandTile(0, 0, null, "Breakers Bridge", frame2.getImage(10));
+        IslandTile BreakersBridge = new IslandTile(0, 0, null, "Breakers Bridge", frame2.getImage(11));
+        IslandTile BreakersBridge = new IslandTile(0, 0, null, "Breakers Bridge", frame2.getImage(12));
+        IslandTile BreakersBridge = new IslandTile(0, 0, null, "Breakers Bridge", frame2.getImage(13));
+        IslandTile BreakersBridge = new IslandTile(0, 0, null, "Breakers Bridge", frame2.getImage(14));
+        IslandTile BreakersBridge = new IslandTile(0, 0, null, "Breakers Bridge", frame2.getImage(15));
+        IslandTile BreakersBridge = new IslandTile(0, 0, null, "Breakers Bridge", frame2.getImage(16));
+        IslandTile BreakersBridge = new IslandTile(0, 0, null, "Breakers Bridge", frame2.getImage(17));
+        IslandTile BreakersBridge = new IslandTile(0, 0, null, "Breakers Bridge", frame2.getImage(18));
+        IslandTile BreakersBridge = new IslandTile(0, 0, null, "Breakers Bridge", frame2.getImage(19));
+        IslandTile BreakersBridge = new IslandTile(0, 0, null, "Breakers Bridge", frame2.getImage(20));
+        IslandTile BreakersBridge = new IslandTile(0, 0, null, "Breakers Bridge", frame2.getImage(21));
+        IslandTile BreakersBridge = new IslandTile(0, 0, null, "Breakers Bridge", frame2.getImage(22));
+        IslandTile BreakersBridge = new IslandTile(0, 0, null, "Breakers Bridge", frame2.getImage(23));
+
 
     }
 
